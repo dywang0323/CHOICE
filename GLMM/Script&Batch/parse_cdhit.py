9d281e5de8959d4d969176b2eb451e36d2fa66f6
Dongyu Wang <60108209+dywang0323@users.noreply.github.com>
2023-07-18 13:49:38 -0500

    Update parse_cdhit.py

--- GLMM/Script&Batch/parse_cdhit.py
+++ GLMM/Script&Batch/parse_cdhit.py
@@ -1,18 +1,14 @@
 # Prepare the culter file (the input in preparing GLMM analysis)
 import re
+import re
 
-cluster_file = "Maternal.clstr"
-out_cluster = "nr40_0.9FFprotein_family"
-out_count = "nr40_0.9FFsubject_count"
-
-f1 = open(out_cluster, 'w')
-f2 = open(out_count, 'w')
+cluster_file = "/ourdisk/hpc/prebiotics/dywang/Projects/CHOICE/Metagenome/GLMM/Infant.clstr"
+output_file = "/ourdisk/hpc/prebiotics/dywang/Projects/CHOICE/Metagenome/GLMM/Infant_cluster_protein_2.txt"
 
 with open(cluster_file) as file_object:
     lines = file_object.readlines()
 
 sum_cluster = {}
-count_subjects = {}
 
 n = 0
 key_term = ''
@@ -25,29 +21,24 @@ for line in lines:
     if groups[0] == ">Cluster 0" and n == 0:
         key_term = "Cluster_0"
         sum_cluster[key_term] = []
-        count_subjects[key_term] = []
         n += 1
         
     elif 'Cluster' in groups[0] and n != 0:
         key_term = re.sub(r'\s+', '_', groups[0])  # Change "Cluster 0" format to "Cluster_0" format
         sum_cluster[key_term] = []
-        count_subjects[key_term] = []
         n += 1
 
     if len(groups) == 3:
         sum_cluster[key_term].append(groups[2])
-        mem = groups[2][0:7]
-        if mem not in count_subjects[key_term]:
-            count_subjects[key_term].append(mem)
 
-f1.write("clusterID\tProteins\n")
+# Filter out clusters with less than 5 proteins
+filtered_clusters = {cluster_id: proteins for cluster_id, proteins in sum_cluster.items() if len(proteins) >= 5}
 
-for name in sorted(count_subjects.keys(), key=lambda x: int(re.search(r'\d+', x).group())):
-    cluster_id = re.sub(r'>', '', name)  # Remove the ">" symbol from clusterID
-    f1.write(cluster_id + "\t" + ",".join([str(x) for x in sum_cluster[name]]) + "\n")
-    f2.write(name + "\t" + ",".join([str(x) for x in sum_cluster[name]]) + "\n")
+with open(output_file, 'w') as f:
+    f.write("clusterID\tProteins\n")
+    for name in sorted(filtered_clusters.keys(), key=lambda x: int(re.search(r'\d+', x).group())):
+        cluster_id = re.sub(r'>', '', name)  # Remove the ">" symbol from clusterID
+        protein_list = ",".join(filtered_clusters[name])
+        f.write(cluster_id + "\t" + protein_list + "\n")
 
-f2.write("There are " + str(len(count_subjects)) + " clusters.\n")
-
-f1.close()
-f2.close()
+    f.write("There are " + str(len(filtered_clusters)) + " clusters with 5 or more proteins.\n")
