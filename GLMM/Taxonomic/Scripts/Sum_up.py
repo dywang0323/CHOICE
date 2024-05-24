59ef56d8a328fa0ddcbe6c4076bdc72342d13d3b
Dongyu Wang <60108209+dywang0323@users.noreply.github.com>
2024-05-24 00:50:26 -0500

    consolidate the values in each column based on the cluster

--- GLMM/Taxonomic/Scripts/Sum_up.py
+++ GLMM/Taxonomic/Scripts/Sum_up.py
@@ -1,4 +1,4 @@
-# This script is used for consolidate the values in each column based on the Taxonomic name
+# This script is used for consolidate the values in each column based on the cluster
 
 import pandas as pd
 
@@ -6,10 +6,14 @@ def consolidate_cluster_values(input_file_path, output_file_path):
     # Load the dataset
     data = pd.read_csv(input_file_path)
     
-    # Group the data by 'Cluster' and calculate the sum for each group
+    # Drop rows where 'Taxa' is empty or '0'
+    data = data[data['Taxa'].astype(str).str.strip() != '']
+    data = data[data['Taxa'].astype(str).str.strip() != '0']
+    
+    # Group the data by 'Taxa' and calculate the sum for each group
     cluster_sum = data.groupby('Taxa').sum()
     
-    # Reset the index to turn 'Cluster' back into a column
+    # Reset the index to turn 'Taxa' back into a column
     cluster_sum.reset_index(inplace=True)
     
     # Save the consolidated data to a CSV file
@@ -18,8 +22,8 @@ def consolidate_cluster_values(input_file_path, output_file_path):
     print(f'Consolidated data has been saved to {output_file_path}')
 
 # Example usage
-input_file_path = '/ourdisk/hpc/prebiotics/dywang/Projects/CHOICE/Metaproteome/Overall/GLMM_taxa/Infant/Infant_taxa.csv'  # Replace with the path to your input CSV file
-output_file_path = '/ourdisk/hpc/prebiotics/dywang/Projects/CHOICE/Metaproteome/Overall/GLMM_taxa/Infant/Infant_taxa_Sum.csv'  # Replace with your desired output CSV file path
+input_file_path = '/ourdisk/hpc/prebiotics/dywang/Projects/CHOICE/Metaproteome/Taxanomic/Maternal/Maternal_taxa.csv'  # Replace with the path to your input CSV file
+output_file_path = '/ourdisk/hpc/prebiotics/dywang/Projects/CHOICE/Metaproteome/Taxanomic/Maternal/Maternal_taxa_Sum.csv'  # Replace with your desired output CSV file path
 
 # Call the function with the paths
 consolidate_cluster_values(input_file_path, output_file_path)
